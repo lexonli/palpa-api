2ea6f36192bb8fb87a0798f9aa475769db88805c
Lexon <lexonli@hotmail.com>
2020-08-11 17:23:15 +1000

    Added proto.js router

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
-  res.send(`Welcome to Palpa API! Hope you enjoy your time here ;)`)
-}
+import handler from '../utils/proto'
+
+const router = handler()
+
+router.get((req, res) => {
+  return res.status(200).send(`Welcome to Palpa API! Hope you enjoy your time here ;)`)
+});
+
+module.exports = router
--- api/version.js
+++ api/version.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
-  res.send(`Version: 1.0.0`);
-}
+import handler from '../utils/proto'
+
+const router = handler()
+
+router.get((req, res) => {
+  return res.status(200).json({ success: true, version: '1.0.0' })
+});
+
+module.exports = router
