363b3a2e276eae244230db8080d1d1fced314407
Lexon <lexonli@hotmail.com>
2020-08-12 17:15:21 +1000

    Removed prettier and added double quotes rule

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-import proto from "@peterjskaltsis/proto";
+import proto from "@peterjskaltsis/proto"
 
-const router = proto();
+const router = proto()
 
 router.get((req, res) => {
   return res
     .status(200)
-    .send('Welcome to Palpa API! Hope you enjoy your time here ;)')
+    .send("Welcome to Palpa API! Hope you enjoy your time here ;)")
 })
 
 module.exports = router
