0da38fe45f1331232879db1b9e1c987b3279e7b3
Lexon <lexonli@hotmail.com>
2020-08-12 16:43:53 +1000

    deleted proto.js and added eslint dev package, ran linter fix

--- api/version.js
+++ api/version.js
@@ -3,7 +3,7 @@ import proto from '@peterjskaltsis/proto'
 const router = proto()
 
 router.get((req, res) => {
-  return res.status(200).json({ success: true, version: "1.0.0" });
-});
+  return res.status(200).json({ success: true, version: '1.0.0' })
+})
 
-module.exports = router;
+module.exports = router
