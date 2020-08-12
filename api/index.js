dd28dca728badab390cf25b5210b6c77d400e273
lexonli <lexonli@users.noreply.github.com>
2020-08-12 06:48:02 +0000

    Prettified Code!

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const router = proto();
 router.get((req, res) => {
   return res
     .status(200)
-    .send('Welcome to Palpa API! Hope you enjoy your time here ;)')
-})
+    .send("Welcome to Palpa API! Hope you enjoy your time here ;)");
+});
 
-module.exports = router
+module.exports = router;
