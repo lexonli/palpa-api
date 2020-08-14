e5163df0dfffbc651e131a82dbc3f219592d05ae
Redrew <andrewshen2913@gmail.com>
2020-08-14 21:00:42 +1000

    set up prettier and airbnb eslint

--- api/version.js
+++ api/version.js
@@ -1,9 +1,9 @@
-import proto from "@peterjskaltsis/proto"
+import proto from '@peterjskaltsis/proto';
 
-const router = proto()
+const router = proto();
 
 router.get((req, res) => {
-  return res.status(200).json({ success: true, version: "1.0.0" })
-})
+  return res.status(200).json({ success: true, version: '1.0.0' });
+});
 
-module.exports = router
+module.exports = router;
