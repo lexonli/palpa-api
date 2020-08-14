dd915197bd77f4792340a0e0d52e5ee9a1657aee
Redrew <andrewshen2913@gmail.com>
2020-08-14 13:51:23 +1000

    added github oauth redirect handler

---
+++ api/oauth/redirect.js
@@ -0,0 +1,58 @@
+
+import proto from "@peterjskaltsis/proto"
+import { CLIENT_ID, CLIENT_SECRET, TOKEN_URL } from "../../config/github"
+const axios = require("axios").default
+
+const router = proto()
+
+function parseParams(str) {
+  const keyValues = str.split("&")
+  const params = {}
+  keyValues.forEach((keyValue, _) => {
+    const [key, value] = keyValue.split("=")
+    params[key] = value
+  })
+  return params
+}
+function parseUrlParams (url) {
+  const idx = url.indexOf("?")
+  if (idx === -1) return {}
+  const query = url.substring(idx + 1)
+  return parseParams(query)
+}
+
+function isEmpty (obj) {
+  return Object.keys(obj).length === 0
+}
+function addParams (url, params) {
+  if (!isEmpty(params)) {
+    const query = Object.entries(params).map(([key, value]) => key + '=' + value).join("&")
+    url = url + "?" + query
+  }
+  return url
+}
+
+router.get((req, res) => {
+  const oauthParams = parseUrlParams(req.url)
+  if ("code" in oauthParams) {
+    const tokenParams = {
+      code: oauthParams.code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
+    }
+    axios.post(addParams(TOKEN_URL, tokenParams), {
+      method: "post",
+    })
+    .then((tokenRes) => {
+      const data = parseParams(tokenRes.data)
+      console.log(data)
+      res.status(200).json({ success: true, token: data.access_token})
+    })
+    .catch((err) => res.status(400).json({ success: false, message: err.toString() }))
+  } else {
+    res.status(400).json({ success: false, message: "No code provided" })
+  }
+  return res
+})
+
+module.exports = router
---
+++ config/github.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CLIENT_ID: "623db91f2020120f6c63",
+  CLIENT_SECRET: "00fdbabee2a51957cf601f2f088efa37500384aa",
+  TOKEN_URL: "https://github.com/login/oauth/access_token"
+}
