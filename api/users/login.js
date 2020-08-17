05d000194e5e09d451c4ec1f90b99a0abd50021d
Lexon <lexonli@hotmail.com>
2020-08-18 01:46:23 +1000

    Added login endpoint and error handling

---
+++ api/users/login.js
@@ -0,0 +1,37 @@
+import faunadb, { query as q } from 'faunadb'
+import proto from '@peterjskaltsis/proto';
+
+const router = proto()
+
+const secret = process.env.FAUNADB_SECRET_KEY
+const client = new faunadb.Client({ secret })
+
+const hasFields = (fields, body) => {
+  return fields.map(field => body[field]).includes(undefined)
+}
+
+router.post((req, res) => {
+  const requestFields = ['email', 'password']
+  try {
+    if (req.body == null) {
+      return res.status(400).json({ errors: ['request body cannot be empty']})
+    }
+    if (hasFields(requestFields, req.body)) {
+      return res.status(400).json({ errors: [`invalid request body, required fields: ${requestFields}`]})
+    }
+    client.query(
+      q.Login(
+        q.Match(q.Index('users_by_email'), req.body.email),
+        { password: req.body.password }
+      )
+    ).then(dbs => {
+      return res.status(200).json({ token: dbs['secret'] })
+    }).catch (error => {
+      return res.status(500).json({ errors : [error.description] })
+    })
+  } catch (error) {
+    return res.status(400).json({ errors : ['json body is malformed'] })
+  }
+})
+
+export default router;
