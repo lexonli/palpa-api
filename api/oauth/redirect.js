1f524e6a465d7398082f0afb4494a18c7a0eccb0
Redrew <andrewshen2913@gmail.com>
2020-08-25 15:39:25 +1000

    added env.GITHUB_SECRET

--- api/oauth/redirect.js
+++ api/oauth/redirect.js
@@ -1,6 +1,8 @@
 import proto from '@peterjskaltsis/proto';
 import axios from 'axios';
-import { CLIENT_ID, CLIENT_SECRET, TOKEN_URL } from '../../config/github';
+import { CLIENT_ID, TOKEN_URL } from '../../config/github';
+
+const CLIENT_SECRET = process.env.GITHUB_SECRET;
 
 const router = proto();
 
