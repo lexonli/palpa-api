88132f8a43774b87736b9c3ecdc5159016514480
Lexon <lexonli@hotmail.com>
2020-08-25 23:45:56 +1000

    further refactoring and commenting on redirect.js

--- api/oauth/redirect.js
+++ api/oauth/redirect.js
@@ -14,15 +14,24 @@ router.get((req, res) => {
     client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
   };
+  fetchGithubAccessToken(tokenParams, res);
+});
+
+/**
+ * Makes a post request to github api with a code to obtain an access token
+ * @param {{code: string, client_id: string, client_secret: string}} tokenParams - code and client credentials from github
+ * @param {object} res - response
+ */
+function fetchGithubAccessToken(tokenParams, res) {
   axios
-    .post(TOKEN_URL, null,  { params: tokenParams })
+    .post(TOKEN_URL, null, { params: tokenParams })
     .then((tokenRes) => {
-      const data = querystring.decode(tokenRes.data)
+      const data = querystring.decode(tokenRes.data);
       return res.status(200).json({ success: true, token: data.access_token });
     })
     .catch((err) => {
-      return res.status(400).json({ success: false, message: err.toString() })
+      return res.status(400).json({ success: false, message: err.toString() });
     });
-});
+}
 
 export default router;
