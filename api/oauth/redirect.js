dd4b47a280b90f3d90e9ab38c516182aa8d2853e
Lexon <lexonli@hotmail.com>
2020-08-25 22:54:41 +1000

    refactored redirect.js

--- api/oauth/redirect.js
+++ api/oauth/redirect.js
@@ -1,53 +1,28 @@
 import proto from '@peterjskaltsis/proto';
 import axios from 'axios';
+import querystring from 'querystring';
 import { CLIENT_ID, CLIENT_SECRET, TOKEN_URL } from '../../config/github';
 
 const router = proto();
 
-function parseParams(str) {
-  const keyValues = str.split('&');
-  const params = {};
-  keyValues.forEach((keyValue) => {
-    const [key, value] = keyValue.split('=');
-    params[key] = value;
-  });
-  return params;
-}
-
-function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
-}
-function addParams(url, params) {
-  if (!isEmpty(params)) {
-    const query = Object.entries(params)
-      .map(([key, value]) => `${key}=${value}`)
-      .join('&');
-    return `${url}?${query}`;
-  }
-  return url;
-}
-
 router.get((req, res) => {
-  if ('code' in req.query) {
-    const tokenParams = {
-      code: req.query.code,
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-    };
-
-    axios
-      .post(addParams(TOKEN_URL, tokenParams))
-      .then((tokenRes) => {
-        const data = parseParams(tokenRes.data);
-        res.status(200).json({ success: true, token: data.access_token });
-      })
-      .catch((err) =>
-        res.status(400).json({ success: false, message: err.toString() })
-      );
-  } else {
-    res.status(400).json({ success: false, message: 'No code provided' });
+  if (!req.query.code) {
+    return res.status(400).json({ success: false, message: 'No code provided' });
   }
-  return res;
+  const tokenParams = {
+    code: req.query.code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+  };
+  axios
+    .post(TOKEN_URL, null,  { params: tokenParams })
+    .then((tokenRes) => {
+      const data = querystring.decode(tokenRes.data)
+      return res.status(200).json({ success: true, token: data.access_token });
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, message: err.toString() })
+    });
 });
 
-module.exports = router;
+export default router;
