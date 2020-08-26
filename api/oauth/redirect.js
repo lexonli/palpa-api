56380b08ea6ef18b28b3b24a3e8b047a386466be
Lexon <lexonli@hotmail.com>
2020-08-27 02:13:15 +1000

    refactored github token flow in redirect.js

--- api/oauth/redirect.js
+++ api/oauth/redirect.js
@@ -2,43 +2,96 @@ import proto from '@peterjskaltsis/proto';
 import axios from 'axios';
 import querystring from 'querystring';
 import { CLIENT_ID, CLIENT_SECRET, TOKEN_URL } from '../../config/github';
+import faunadb, { query as q } from 'faunadb';
 
 /**
  * Makes a post request to github api with a code to obtain an access token
- * @param {{code: string, client_id: string, client_secret: string}} tokenParams
  * - code and client credentials from github
- * @param {object} res - response
+ * @param {string} code - Code returned from frontend
+ * @param {string} client_id - ClientId from github
+ * @param {string} client_secret - Client Secret from github
  */
-function fetchGithubAccessToken(tokenParams, res) {
-  axios
-    .post(TOKEN_URL, null, { params: tokenParams })
+function fetchGithubToken(code, client_id, client_secret) {
+  return axios
+    .post(TOKEN_URL, null, { params: {
+        code: code,
+        client_id: client_id,
+        client_secret: client_secret,
+      }
+    })
     .then((tokenRes) => {
       const data = querystring.decode(tokenRes.data);
-      if (data.error) {
-        return res.status(400).json({ success: false, message: data.error });
-      }
-      return res.status(200).json({ success: true, token: data.access_token });
+      // if (data.error) {
+      //   throw data.error;
+      // }
+      data.access_token = '123token';
+      return data.access_token
     })
-    .catch((err) => {
-      return res.status(400).json({ success: false, message: err.toString() });
-    });
+}
+
+function storeGithubToken(ref, token) {
+  return client.query(
+    q.Update(
+      q.Ref(q.Collection('users'), ref),
+      { data: { github_token: token } },
+    )
+  ).then(() => token)
+}
+
+
+function verifyToken(req, res) {
+  const bearerHeader = req.headers['authorization'];
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(' ');
+    req.token = bearer[1];
+  } else {
+    res.status(403).json({ success: false, message: 'No authentication token provided'});
+  }
+}
+
+function getClient(req, res) {
+  if (!req.token) {
+    res.status(400).json({ success: false, message: 'No authentication token provided'});
+  }
+  const secret = req.token;
+  return new faunadb.Client({ secret });
+}
+
+/**
+ * Authenticates the client and obtains the document ref if ok
+ * @param client - faunadb client instantiated with secret
+ * @returns {*|Promise<Object>|Promise<PermissionStatus>} - Promise with document ref if successful
+ */
+function authenticate(client) {
+  return client.query(q.Identity())
 }
 
 const router = proto();
+let client;
 
 router.get((req, res) => {
-  if (!req.query.code) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'No code provided' });
-  }
-  const tokenParams = {
-    code: req.query.code,
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
-  };
-  fetchGithubAccessToken(tokenParams, res);
-  return res;
+  //TODO: Move to middleware
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  verifyToken(req, res);
+  client = getClient(req, res);
+
+  authenticate(client)
+    .then((authInfo) => {
+      if (!req.query.code) {
+        throw 'No code provided';
+      }
+      return fetchGithubToken(req.query.code, CLIENT_ID, CLIENT_SECRET)
+        .then((token) => [token, authInfo.id])
+    })
+    .then(([token, ref]) => {
+      return storeGithubToken(ref, token)
+    })
+    .then((token) => {
+      res.status(200).json({ success: true, message: token })
+    })
+    .catch((error) => {
+      res.status(400).json({ success: false, message: error.toString() })
+    });
 });
 
 export default router;
