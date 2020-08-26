4f1cbaf93b86aa3dce95d7f73b9bf6c001ef239c
Redrew <andrewshen2913@gmail.com>
2020-08-26 21:30:09 +1000

    fixed small linting issues

--- config/aws.js
+++ config/aws.js
@@ -1,6 +1,6 @@
 module.exports = {
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  S3_IMAGE_BUCKET: "palpa-images-test",
+  S3_IMAGE_BUCKET: 'palpa-images-test',
   S3_EXPIRE_SECONDS: 60 * 5,
 };
