710838703201097dfbe3e8f2e829b2cca1c51a09
Redrew <andrewshen2913@gmail.com>
2020-08-26 21:05:41 +1000

    set up config with env variables

---
+++ config/frontend.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ROOT: "http://localhost:3000"
+}
--- config/github.js
+++ config/github.js
@@ -1,4 +1,5 @@
 module.exports = {
   CLIENT_ID: '623db91f2020120f6c63',
   TOKEN_URL: 'https://github.com/login/oauth/access_token',
+  CLIENT_SECRET: process.env.GITHUB_SECRET,
 };
