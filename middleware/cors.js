acf5ef5c135560c38255a702c7365c192a79cb42
Lexon <lexonli@hotmail.com>
2020-09-05 13:49:49 +1000

    Added updated cors middleware with options preflight api

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,15 @@
 function cors(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   next();
 }
 
