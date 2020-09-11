2d776402b2db65f6e1b3a86f337fc74d737ffbbf
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-09-11 18:07:00 +1000

    Fix create project API and linting

--- api/project/index.js
+++ api/project/index.js
@@ -1,35 +1,35 @@
-import nc from 'next-connect'
-import cors from '../../middleware/cors'
-import { getUserFromUsername } from '../../controllers/user'
-import { getProjectsFromUserId } from '../../controllers/project'
-import validator from '../../middleware/validator'
-import { usernameSchema } from '../../models/user'
-import { projectSchema } from '../../models/project'
+import nc from 'next-connect';
+import cors from '../../middleware/cors';
+import { getUserFromUsername } from '../../controllers/user';
+import { getProjectsFromUserId } from '../../controllers/project';
+import validator from '../../middleware/validator';
+import { usernameSchema } from '../../models/user';
+import { projectSchema } from '../../models/project';
 
-const router = nc()
-router.use(cors)
+const router = nc();
+router.use(cors);
 
 router.get(validator(usernameSchema, 'query'), (req, res) => {
-  const { username } = req.query
+  const { username } = req.query;
   getUserFromUsername(username)
     .then((user) => getProjectsFromUserId(user))
     .then((projects) =>
       res.status(200).json({
         projects,
-      }),
+      })
     )
     .catch((error) =>
       res.status(400).json({
         errors: [{ message: error.toString() }],
-      }),
-    )
-})
+      })
+    );
+});
 
 router.post((req, res) => {
-  const { projectName } = req.query
+  const { projectName } = req.body;
   // Check if project name is available
-  res.status(200).json(projectName)
+  res.status(200).json({ projectName });
   // save the project to fauna
-})
+});
 
-export default router
+export default router;
