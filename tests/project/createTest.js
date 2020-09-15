d89d5689f6836c6d64f2cd580b96fbafd16ce843
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-09-15 14:45:18 +1000

    Create test skeleton

---
+++ tests/project/createTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const api = require('../../api/project/index');
+
+// template for a unittest
+// it('', function () {});
+
+describe('Test the create endpoint of project api', function () {
+  it('create api should return status code 200 with valid request body', function () {});
+});
