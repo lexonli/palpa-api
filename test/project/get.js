8618c8cf4db15468be1a5240110a7aa740c5f43d
Peteski <38994044+YAOS5@users.noreply.github.com>
2020-09-16 17:06:14 +1000

    Create status 400 test for get project by ID

--- test/project/get.js
+++ test/project/get.js
@@ -15,7 +15,7 @@ const apiUrl = process.env.API_URL;
 describe('Test the create endpoint of project api', function () {
   // disable timeouts
   this.timeout(0);
-  it('Get project data by project ID with valid ID', function (done) {
+  it('200, Get project data by project ID with valid ID', function (done) {
     const projectID = '273904644720165389';
     chai
       .request(apiUrl)
@@ -24,6 +24,19 @@ describe('Test the create endpoint of project api', function () {
       .end((err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body).to.contain.property('project');
+        done();
+      });
+  });
+
+  it('400, Get project data by project ID with non-existant ID', function (done) {
+    const wrongProjectID = '2739046';
+    chai
+      .request(apiUrl)
+      .get(`/project/${wrongProjectID}`)
+      .set('content-type', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body).to.contain.property('errors');
         done();
       });
   });
